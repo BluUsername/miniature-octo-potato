a86b6bff21b5a85cfc0362606211c646a9d1e9e3
BluUsername <iamtomherman@gmail.com>
2025-06-25 15:24:01 +0100

    Update JS

--- script.js
+++ script.js
@@ -112,13 +112,6 @@ const questions = [
   }
 ];
 
-// Shuffle questions for each quiz attempt
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
 
 let shuffledQuestions, currentQuestion, score, userAnswers;
 
